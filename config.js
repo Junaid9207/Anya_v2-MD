af4f633299162a88e65e36376c58edb9fed1bc4b
Junaid9207 <junaidjunu90211@gmail.com>
2024-09-04 16:32:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "3UETVfi2C9imyzn15ZH51p2Zq0CDKuA1jxZqHp148CRrJtxsyfGm71sPsHK1nZHN",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://<db_junaid>:<db_eLWkJ5sKheboFEgK>@cluster0.ysvhj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     sessionId: process.env.SESSION_ID || "OWc0d_Queen-Anya_nd_Queen-Anya_nMzI=",
 
     /**
